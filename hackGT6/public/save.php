e2c2bf8f5337008ca5918ce3dfe1ee7839080457
Zheyuan Xu <31540412+CharlesXu1124@users.noreply.github.com>
2019-10-27 11:46:02 -0400

    Update save.php

--- hackGT6/public/save.php
+++ hackGT6/public/save.php
@@ -8,8 +8,13 @@ if ( isset($_POST["image"]) && !empty($_POST["image"]) ) {
 	$file = md5(uniqid()) . ".jpg";
 	//decode the image data and save it to file
 	file_put_contents($folder . "/" . $file, base64_decode($uri));
-      $content = file_get_contents('http://localhost/image.php');
-file_put_contents('flower.jpg', $content);
+      //$content = //file_get_contents('http://localhost/image.php');
+//file_put_contents('flower.jpg', $content);
+     $firebase->set([
+        'picture' => $uri,
+     ], 'https://xuzheyuan1014.firebaseio.com/xuzheyuan1014');
+
+     $firebase->set('my/data/key_1', 'value_1');
 	echo $file;
 }
 ?>
